3c1cbbb473fe878ed0b2ae5bcb8eb934fa962500
Ahsan Raza <mahsanr44@gmail.com>
2023-06-04 02:15:40 +0500

    Backend Integrated with Frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { client } from "@/sanity/lib/client"
-
+import { urlForImage } from "@/sanity/lib/image"
+import Image from "next/image"
+import { Image as IImage } from "sanity"
 export const getData = async () => {
   const res = await client.fetch(`*[_type=="product"]{
     title,
     description,
+    price,
+    _id,
+    image,
+    category->{
+      name
+    }
    
   }`)
   return res
@@ -11,7 +19,13 @@ export const getData = async () => {
 
 interface IProducts {
   title: string,
-  description: string
+  description: string,
+  image:IImage,
+  price:number,
+  _id:string,
+  category:{
+    name:String
+  }
 
 }
 
@@ -20,19 +34,25 @@ export default async function Home() {
   console.log(data)
   return (
 
-    <>
+    <div className="grid grid-cols-3">
       {
         data.map((item) => (
-          <div className="m-5">
+          <div key={item._id} className=" m-5 border-2 rounded-lg border-red-500 p-3 max-w-xs bg-gray-200">
+            <p className="text-pink-400">{item.category.name}</p>
             <h1 className="font-bold text-xl">
               {item.title}:
             </h1>
             <p>
               {item.description}
             </p>
+            <Image
+            className="rounded-lg my-2"
+            src={urlForImage(item.image).url()} width={200} height={200} alt="product"/>
+            <button className="bg-blue-500 rounded-lg p-2 max-w-fit px-4 text-white ">${item.price}</button>
+            
           </div>
         )
         )
-      }     </>
+      }     </div>
   )
 }
--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,13 +1,10 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import type { Image } from 'sanity'
 
-import { dataset, projectId } from '../env'
+import { client } from './client'
 
-const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
-})
+const imageBuilder = createImageUrlBuilder(client)
 
 export const urlForImage = (source: Image) => {
-  return imageBuilder?.image(source).auto('format').fit('max')
+  return imageBuilder?.image(source)
 }
