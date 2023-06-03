d5bc70624bd2d01a2e63b562e9b0a668b7b28445
Ahsan Raza <mahsanr44@gmail.com>
2023-06-04 02:59:46 +0500

    Add to Cart APIs Completed

---
+++ app/api/cart/route.ts
@@ -0,0 +1,42 @@
+import { cartTable, db } from "@/sanity/lib/drizzle";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuid } from "uuid";
+
+export const GET = async (request: NextRequest) => {
+  try {
+    const res = await db.select().from(cartTable);
+    return NextResponse.json({
+      data: res,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: error,
+    });
+  }
+};
+
+export const POST = async (request: NextRequest) => {
+  const req = await request.json();
+
+  const uid = uuid();
+  const setCookies = cookies();
+  if (!cookies().has("user_id")) {
+    setCookies.set("user_id", uid);
+  }
+
+  try {
+    const res = await db.insert(cartTable).values({
+      product_id: req.product_id,
+      quantity: req.quantity,
+      user_id: cookies().get('user_id')?.value as string,
+    });
+    return NextResponse.json({
+      data: res,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: error,
+    });
+  }
+};
